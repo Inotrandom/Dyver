bb611312920387fdc713f0b00db95e8e2726d27e
Estelle <yacoonan@gmail.com>
2026-03-04 16:18:21 -0500

    docs: renderobj

--- src/ui/renderobj.h
+++ src/ui/renderobj.h
@@ -9,6 +9,12 @@ public:
 	explicit renderobj_t() {}
 	~renderobj_t() {}
 
+	/**
+	 * @brief Render this renderable
+	 *
+	 * @param renderer SDL renderer used to render geometry
+	 * @param window Window used to extrapolate the relative size of geometry
+	 */
 	virtual void render(SDL_Renderer *renderer, SDL_Window *window) = 0;
 
 private:
